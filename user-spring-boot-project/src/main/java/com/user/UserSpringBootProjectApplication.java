b680a8d36b017911fb685741232baf8aa29202ab
Divya <Divya@LAPTOP-HIUPKHJ9>
2020-09-15 11:56:41 +0530

    Spring-Boot-User-Project

---
+++ user-spring-boot-project/src/main/java/com/user/UserSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSpringBootProjectApplication.class, args);
+	}
+
+}
